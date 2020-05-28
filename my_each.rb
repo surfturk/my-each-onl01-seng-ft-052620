be7581143c2bc7785a466d8e7382fbc8d7b861fe
Berkley Brown <berkleydeane@gmail.com>
2020-05-28 15:27:43 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
-  # code here
-end
+def my_each(array)
+   counter = 0
+   while counter < array.length
+    yield array[counter]
+    counter += 1
+   end
+   array
+  end
+
