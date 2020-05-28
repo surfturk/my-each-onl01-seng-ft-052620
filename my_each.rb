3ce20bd3eda1d9007616e73e1b1f31501d26f84b
Berkley Brown <berkleydeane@gmail.com>
2020-05-28 15:13:15 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,7 @@
 def my_each # put argument(s) here
-  my_each(collection) do |i|
-  puts i
-end
+  
 end
 
-my_each(collection) do |i|
-  puts i
-end
+##my_each(collection) do |i|
+##  puts i
+##end
